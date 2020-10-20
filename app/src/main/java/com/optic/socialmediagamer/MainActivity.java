dfbdb367c0e3438bc423b845558671f53e2a636a
Luis Fernando Guerra <luisjeremias33.3@gmail.com>
2020-10-19 20:12:10 -0500

    Reglas seguras para la base de datos

---
+++ app/src/main/java/com/optic/socialmediagamer/MainActivity.java
@@ -0,0 +1,53 @@
+package com.optic.socialmediagamer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView mTextViewRegister;
+    TextInputEditText mTextInputEmail;
+    TextInputEditText mTextInputPassword;
+    Button mButtonLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextViewRegister = findViewById(R.id.textViewRegister);
+        mTextInputEmail = findViewById(R.id.textInputEmail);
+        mTextInputPassword = findViewById(R.id.textInputPassword);
+        mButtonLogin = findViewById(R.id.btnLogin);
+
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+
+        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void login() {
+        String email = mTextInputEmail.getText().toString();
+        String password = mTextInputPassword.getText().toString();
+        Log.d("CAMPO", "email: " + email);
+        Log.d("CAMPO", "password: " + password);
+    }
+}
